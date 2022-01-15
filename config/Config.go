11b540348856ab436e7bf5aead40d3bdaf24dd0f
Jan Thewes <janthewes@icloud.com>
2022-01-15 10:46:00 +0100

    Set CodeQL language to go

--- config/Config.go
+++ config/Config.go
@@ -11,33 +11,33 @@ var (
 	HueShellyConfig Config
 )
 
-type Config struct ***REMOVED***
+type Config struct {
 	HueBridgeIp string `json:"hueBridgeIp"`
 	HueUser     string `json:"hueUser"`
 	ServerPort  int    `json:"serverPort"`
-***REMOVED***
+}
 
-func (config Config) New() ***REMOVED***
+func (config Config) New() {
 	logging.Logger.Println("Reading config from config.json")
 	jsonFile, err := os.Open("config.json")
-	if err != nil ***REMOVED***
+	if err != nil {
 		handleConfigReadError(err)
-	***REMOVED***
+	}
 	jsonContent, err := ioutil.ReadAll(jsonFile)
 	err = json.Unmarshal(jsonContent, &HueShellyConfig)
 	handleConfigReadError(err)
-	if err != nil ***REMOVED***
+	if err != nil {
 		handleConfigReadError(err)
-	***REMOVED***
+	}
 	err = jsonFile.Close()
-	if err != nil ***REMOVED***
+	if err != nil {
 		handleConfigReadError(err)
-	***REMOVED***
-***REMOVED***
+	}
+}
 
-func handleConfigReadError(err error) ***REMOVED***
-	if err != nil ***REMOVED***
+func handleConfigReadError(err error) {
+	if err != nil {
 		logging.Logger.Println("Error reading config.json. Please make sure file exists and is valid.")
 		logging.Logger.Fatalln(err)
-	***REMOVED***
-***REMOVED***
+	}
+}
